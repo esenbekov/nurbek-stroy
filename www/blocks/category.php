032d4441082f4d1b001d821c62103d3db3e456ab
Islam Esenbekov <muni.esenbekov@gmail.com>
2021-03-10 21:21:34 +0600

    added catalog

---
+++ www/blocks/category.php
@@ -0,0 +1,108 @@
+<div class="site-container clear-self">
+
+    <div class="site-container-in clear-self" style="opacity: 1;">
+
+        <main role="main" class="site-main">
+            <div class="site-main-inner">
+                <div class="main-slider clear-self">
+                    <div class="owl-carousel owl-theme owl-loaded">
+                        <div class="owl-stage-outer">
+                            <div class="owl-stage" style="transform: translate3d(-1923px, 0px, 0px); transition: all 0.25s ease 0s; width: 3846px;">
+                                <div class="owl-item cloned" style="width: 641px; margin-right: 0px;">
+                                    <div class="slider-img"><img src="/thumb/2/LnSV4XmorkPs_pokkaSnjg/940r425/d/banner.jpg" alt=""></div>
+                                </div>
+                                <div class="owl-item cloned" style="width: 641px; margin-right: 0px;">
+                                    <div class="slider-img"><img src="/thumb/2/LnSV4XmorkPs_pokkaSnjg/940r425/d/banner.jpg" alt=""></div>
+                                </div>
+                                <div class="owl-item" style="width: 641px; margin-right: 0px;">
+                                    <div class="slider-img"><img src="/thumb/2/LnSV4XmorkPs_pokkaSnjg/940r425/d/banner.jpg" alt=""></div>
+                                </div>
+                                <div class="owl-item active" style="width: 641px; margin-right: 0px;">
+                                    <div class="slider-img"><img src="/thumb/2/LnSV4XmorkPs_pokkaSnjg/940r425/d/banner.jpg" alt=""></div>
+                                </div>
+                                <div class="owl-item cloned" style="width: 641px; margin-right: 0px;">
+                                    <div class="slider-img"><img src="/thumb/2/LnSV4XmorkPs_pokkaSnjg/940r425/d/banner.jpg" alt=""></div>
+                                </div>
+                                <div class="owl-item cloned" style="width: 641px; margin-right: 0px;">
+                                    <div class="slider-img"><img src="/thumb/2/LnSV4XmorkPs_pokkaSnjg/940r425/d/banner.jpg" alt=""></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="shop2-cookies-disabled shop2-warning hide"></div>
+
+
+                <div class="shop-main-header " style="opacity: 1;">
+                    Магазин
+                </div>
+                <div class="main-new-block-wrapper" style="opacity: 1;">
+                    <div class="main-new-block">
+                        <div class="main-blocks-slider owl-carousel owl-theme owl-loaded">
+                            <div class="owl-stage-outer">
+                                <div class="owl-stage"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="shop2-main-before">
+                    <p>Компания Нурбек-строй&nbsp;была основана в 2012 году с целью обеспечения кыргызских потребителей качественными строительными и отделочными материалами от ведущих европейских производителей по доступным ценам. &nbsp;</p>
+
+                    <p>Компания производит продажу:</p>
+
+                    <ul>
+                        <li>
+                            <p>Материалов для внутренних и фасадных работ;</p>
+                        </li>
+                        <li>
+                            <p>Материалов для гидроизоляции;</p>
+                        </li>
+                        <li>
+                            <p>Добавок в бетон;</p>
+                        </li>
+                        <li>
+                            <p>Пластификаторов;</p>
+                        </li>
+                        <li>
+                            <p>Промышленных полов;</p>
+                        </li>
+                        <li>
+                            <p>Сухих смесей и грунтовок;</p>
+                        </li>
+                        <li>
+                            <p>Напольных покрытий;</p>
+                        </li>
+                        <li>Спортивных покрытий;</li>
+                    </ul>
+
+                    <p>и многого другого, что повседневно необходимо строителям в их работе.</p>
+                </div>
+            </div>
+        </main> <!-- .site-main -->
+        <aside role="complementary" class="site-sidebar left">
+            <div class="left-categor-wr">
+                <div class="left-categor-title">Каталог<span class="s1"><span class="s2"></span></span></div>
+                <ul class="left-categor">
+                    <li><a href="/shop/folder/stroitelnye-materialy">Строительные материалы</a></li>
+                    <li><a href="/shop/folder/suhie-smesi-i-gruntovki">Клеи</a></li>
+                    <li><a href="/shop/folder/epoksidnye-i-topping-poly">Эпоксидные и топпинг полы</a></li>
+                    <li><a href="/shop/folder/sportivnoe-pokrytiya">Спортивное покрытия</a></li>
+                    <li><a href="/shop/folder/napolnye-pokrytiya">Коммерческие покрытия</a></li>
+                    <li><a href="/shop/folder/sad">Сад</a></li>
+                    <li><a href="/shop/folder/kovroliny-i-kovrovye-plitki">Ковролины и ковровые плитки</a></li>
+                    <li><a href="/shop/folder/kraski">Краски</a></li>
+                    <li><a href="/shop/folder/elektrotovary">Электротовары</a></li>
+                    <li><a href="/shop/folder/vodosnabzhenie">Водоснабжение</a></li>
+                    <li><a href="/shop/folder/santehnika">Сантехника</a></li>
+                    <li><a href="/shop/folder/skobyanye-izdeliya">Скобяные изделия</a></li>
+                    <li><a href="/shop/folder/dobavki-v-beton">Добавки в бетон</a></li>
+                    <li><a href="/shop/folder/instrumenty">Инструменты</a></li>
+                    <li><a href="/shop/folder/specialnaya-odezhda">Специальная одежда</a></li>
+                    <li><a href="/shop/folder/820083261">Освещение</a></li>
+                </ul>
+            </div>
+        </aside> <!-- .site-sidebar.left -->
+        <div class="clear-mid clear-self"></div>
+    </div>
+</div>
--- www/index.php
+++ www/index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title>"Нурбек Cтрой" - строительный магазин</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <?php include("blocks/header.php"); ?>
+    <?php include("blocks/category.php"); ?>
 </body>
+
 </html>
